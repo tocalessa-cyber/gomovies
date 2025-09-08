e1aaf1a835d5dab8a29fb950553422f826a6b183
musadek akhmad <musadekakhmad@gmail.com>
2025-09-08 07:53:34 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -79,7 +79,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen p-8 bg-slate-900 text-white">
-      <h1 className="text-5xl md:text-6xl font-extrabold text-center text-orange-400 mb-4 drop-shadow-lg">
+      <h1 className="text-5xl md:text-6xl font-extrabold text-center text-blue-400 mb-4 drop-shadow-lg">
         Himovies
       </h1>
       <p className="text-xl md:text-2xl text-center text-gray-300 mb-12">
