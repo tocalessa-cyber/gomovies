65211a977db01fc8fed7a69c18f5c17ff03549b2
musadek akhmad <musadekakhmad@gmail.com>
2025-09-08 09:18:54 +0700

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -5,7 +5,26 @@ export default function Footer() {
     <footer className="bg-slate-900 text-gray-400 py-6 text-center rounded-lg shadow-xl">
       <div className="container mx-auto px-4">
         <p>&copy; {year} Himovies. All rights reserved.</p>
-        <p className="mt-2 text-sm">Powered by TMDB API & Next.js</p>
+        <p className="mt-2 text-sm">
+          Powered by{' '}
+          <a
+            href="https://www.themoviedb.org/"
+            className="text-blue-500 hover:text-blue-400 transition-colors duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TMDB API
+          </a>{' '}
+          & Next.js{' '}
+          <a
+            href="https://himovies-us.netlify.app/"
+            className="text-blue-500 hover:text-blue-400 transition-colors duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Himovies
+          </a>
+        </p>
       </div>
     </footer>
   );
